aad76a1f05c22923caa0e70e82832df92da8e102
mccamatt92 <matthias.mccarthy@gmail.com>
2020-11-12 18:40:20 +0100

    Update Server js

---
+++ tinder-backend/server.js
@@ -0,0 +1,8 @@
+import express  from 'express'
+import  Mongoose  from 'mongoose'
+
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/', (req, res) => res.status(200).send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
